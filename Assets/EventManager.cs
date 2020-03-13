9176d83a59facf14f9748d29e6b32a29de417af6
CobaltBlack <007.ericliu@gmail.com>
2020-03-13 00:01:23 -0700

    moved some files

--- Assets/EventManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EventManager : MonoBehaviour
-{
-    public delegate void MapAction(Vector2 mapPos);
-    public static event MapAction OnMapAction;
-}
